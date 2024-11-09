96b26a8b659281f618e671ef76b82729acfd18c3
Richard Jérémy <jeremy.rc77@gmail.com>
2024-11-09 17:52:05 +0700

    s

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,13 +8,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Notifications\ResetPasswordNotification;
-
+use Laravel\Sanctum\HasApiTokens;
 
 
 class User extends Authenticatable implements CanResetPassword
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
