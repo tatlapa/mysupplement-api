a08986df9e57e5bd944bb9d31c4cba18f01b6161
Richard Jérémy <jeremy.rc77@gmail.com>
2025-05-08 17:12:21 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,5 @@ php artisan config:cache
 echo "⚙️ Running migrations..."
 php artisan migrate --force
 
-echo "🌱 Running seeders..."
-php artisan db:seed --force
-
 echo "🚀 Starting Laravel development server..."
 exec php artisan serve --host=0.0.0.0 --port=8000
